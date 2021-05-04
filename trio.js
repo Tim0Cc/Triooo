251a59da4d3db761e8adf0fac74759c6f5af2724
Tim Cecatka <tim.cecatka@gmail.com>
2021-05-04 23:10:34 +0200

    function createTiles works

--- trio.js
+++ trio.js
@@ -1,5 +1,17 @@
-function tilesArray() {
-  console.log("from tiles array");
+const tilesArray = []
+
+function createTiles() {
+  console.log(tilesArray);
+  for (i=0; i<4; i++) {
+    tilesArray.push("9")
+  }
+  for (i=0; i<5; i++) {
+    tilesArray.push("7", "8")
+  }
+  for (i=0; i<6; i++) {
+    tilesArray.push("2", "3", "4", "5", "6")
+  }
+  console.log(tilesArray);
 }
 
-tilesArray();
+createTiles();
