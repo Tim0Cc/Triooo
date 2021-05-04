c12406bc5839a2328ec4168a13c52f8e1966dd9c
Tim Cecatka <tim.cecatka@gmail.com>
2021-05-04 23:42:34 +0200

    function shuffleArray()

--- trio.js
+++ trio.js
@@ -8,22 +8,39 @@ function createTiles() {
     tilesArray.push("9")
   }
   for (i=0; i<5; i++) {
-    tilesArray.push("7", "8")
+    tilesArray.push("1", "7", "8")
   }
   for (i=0; i<6; i++) {
     tilesArray.push("2", "3", "4", "5", "6")
   }
-  console.log(tilesArray);
+  console.log("tiles Array: " + tilesArray);
+  console.log("Number of Tiles: " + tilesArray.length);
+  
 }
 
 createTiles();
+
+// shuffle Tiles
+
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+const shuffledArray = shuffle(tilesArray);
+console.log("shuffled Array: " + shuffledArray);
+console.log("Number of Tiles: " + shuffledArray.length);
 
 // fill HTML table
 
 const table = document.querySelector("table");
 
 function fillHTML() {
-  table.innerHTML = "Hello from Query";
+
+  table.innerHTML = shuffledArray;
 }
 
 fillHTML();
